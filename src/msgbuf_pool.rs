42a6b0e04522713c8956536a9289df904da46b35
ben-han-cn <ben.han.cn@gmail.com>
2020-06-11 10:29:59 +0000

    use mio and memory channel instead of future

---
+++ src/msgbuf_pool.rs
@@ -0,0 +1,82 @@
+use std::mem;
+
+const MAX_BUF_LEN: usize = 512;
+
+#[derive(Debug)]
+pub struct MessageBuf {
+    pub data: Box<[u8]>,
+    pub len: usize,
+}
+
+type Slot = Option<(MessageBuf, usize)>;
+
+pub struct MessageBufPool {
+    slots: Vec<Slot>,
+    len: usize,
+    next: usize,
+}
+
+impl MessageBufPool {
+    pub fn new(len: usize) -> Self {
+        let mut slots = (0..len).fold(Vec::with_capacity(len), |mut v, i| {
+            v.push(Some((
+                MessageBuf {
+                    data: Box::new([0; MAX_BUF_LEN]),
+                    len: 0,
+                },
+                i + 1,
+            )));
+            v
+        });
+
+        MessageBufPool {
+            slots,
+            len,
+            next: 0,
+        }
+    }
+
+    pub fn allocate(&mut self) -> Option<MessageBuf> {
+        if self.next == self.len {
+            None
+        } else {
+            let prev = mem::replace(&mut self.slots[self.next], None);
+            if let Some((buf, next)) = prev {
+                self.next = next;
+                return Some(buf);
+            }
+            panic!("next should point to usable buf");
+        }
+    }
+
+    pub fn release(&mut self, mut buf: MessageBuf) {
+        if let Some(free_index) = self.slots.iter().position(|s| s.is_none()) {
+            buf.len = 0;
+            self.slots[free_index] = Some((buf, self.next));
+            self.next = free_index;
+        } else {
+            panic!("release buf not allocate by this pool");
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_allocate_and_release() {
+        let mut pool = MessageBufPool::new(2);
+        let mut buf = pool.allocate().unwrap();
+        buf.data[0] = 1;
+        pool.release(buf);
+
+        let buf1 = pool.allocate().unwrap();
+        assert_eq!(buf1.data[0], 1);
+        let mut buf2 = pool.allocate().unwrap();
+        buf2.data[0] = 2;
+        assert!(pool.allocate().is_none());
+        pool.release(buf2);
+        let buf = pool.allocate().unwrap();
+        assert_eq!(buf.data[0], 2);
+    }
+}
--- src/resolver.rs
+++ src/resolver.rs
@@ -1,51 +1,84 @@
-use std::future::Future;
-use std::pin::Pin;
-use std::sync::{Arc, RwLock};
+use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+use std::{io, thread};
 
-use crate::auth::{AuthServer, AuthZone};
-use crate::config::VanguardConfig;
-use crate::iterator::{new_iterator, Iterator};
-use crate::types::{Handler, Request, Response};
 use anyhow;
+use crossbeam::channel::bounded;
+use mio::net::UdpSocket;
+use mio::{Events, Interest, Poll, Token};
+
+use crate::auth::AuthServer;
+use crate::config::VanguardConfig;
+use crate::msgbuf_pool::{MessageBuf, MessageBufPool};
+use crate::types::{Request, Response};
+
+const UDP_SOCKET: Token = Token(0);
 
 #[derive(Clone)]
 pub struct Resolver {
     auth_server: AuthServer,
-    iterator: Iterator,
 }
 
 impl Resolver {
     pub fn new(config: &VanguardConfig) -> Self {
         let auth_server = AuthServer::new(&config.auth);
-        Resolver {
-            auth_server,
-            iterator: new_iterator(config),
+        Resolver { auth_server }
+    }
+
+    pub fn run(&self) {
+        let (req_sender, resp_receiver) = bounded::<(MessageBuf, SocketAddr)>(1024);
+        let mut poll = Poll::new().unwrap();
+        let mut events = Events::with_capacity(1);
+        let addr = "0.0.0.0:53".parse().unwrap();
+        let mut socket = UdpSocket::bind(addr).unwrap();
+        poll.registry()
+            .register(&mut socket, UDP_SOCKET, Interest::READABLE)
+            .unwrap();
+        let msgbuf_pool = Arc::new(Mutex::new(MessageBufPool::new(1024)));
+        let cpus = num_cpus::get();
+        let worker_thread_count = if cpus > 2 { cpus - 2 } else { 1 };
+        let socket = Arc::new(socket);
+        for i in (0..worker_thread_count) {
+            thread::spawn({
+                let msgbuf_pool = msgbuf_pool.clone();
+                let socket_sender = socket.clone();
+                let resp_receiver = resp_receiver.clone();
+                move || loop {
+                    if let Ok((buf, addr)) = resp_receiver.recv() {
+                        socket_sender.send_to(&buf.data[..buf.len], addr);
+                        msgbuf_pool.lock().unwrap().release(buf);
+                    }
+                }
+            });
         }
-    }
 
-    pub fn zone_data(&self) -> Arc<RwLock<AuthZone>> {
-        self.auth_server.zone_data()
-    }
-
-    async fn do_resolve(&mut self, req: Request) -> anyhow::Result<Response> {
-        if let Some(response) = self.auth_server.resolve(&req) {
-            return Ok(Response::new(response));
+        loop {
+            poll.poll(&mut events, None).unwrap();
+            let mut drop = [0; 512];
+            for event in events.iter() {
+                match event.token() {
+                    UDP_SOCKET => loop {
+                        let maybe_buf = msgbuf_pool.lock().unwrap().allocate();
+                        if let Some(mut buf) = maybe_buf {
+                            if let Ok((len, addr)) = socket.recv_from(&mut buf.data) {
+                                buf.len = len;
+                                req_sender.try_send((buf, addr));
+                            } else {
+                                msgbuf_pool.lock().unwrap().release(buf);
+                                break;
+                            }
+                        } else {
+                            if let Ok((len, addr)) = socket.recv_from(&mut drop) {
+                            } else {
+                                break;
+                            }
+                        }
+                    },
+                    _ => {
+                        println!("Got event for unexpected token: {:?}", event);
+                    }
+                }
+            }
         }
-
-        self.iterator.resolve(req).await
-    }
-
-    async fn echo(&mut self, req: Request) -> anyhow::Result<Response> {
-        Ok(Response::new(req.request))
-    }
-}
-
-impl Handler for Resolver {
-    fn resolve(
-        &mut self,
-        req: Request,
-    ) -> Pin<Box<dyn Future<Output = anyhow::Result<Response>> + Send + '_>> {
-        //Box::pin(self.do_resolve(req))
-        Box::pin(self.echo(req))
     }
 }
